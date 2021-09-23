17c26924047c766a1d0f12dd8bd8f2c659134ca8
SimBoex <91120377+SimBoex@users.noreply.github.com>
2021-09-23 23:27:10 +0200

    Update scripts.py

--- scripts.py
+++ scripts.py
@@ -1,2 +1,33 @@
 #Say "Hello, World!" With Python
 print("Hello, World!")
+
+#Arithmetic Operators
+if __name__ == '__main__':
+    a = int(raw_input())
+    b = int(raw_input())
+    print(a+b)
+    print(a-b)
+    print(a*b)
+    
+#Python If-Else
+if __name__ == '__main__':
+    n = int(raw_input().strip())
+    if n%2!=0: #odd
+        print("Weird")
+    else:
+        if n>20:
+            print("Not Weird")
+        elif n>=6:
+            print("Weird")
+        else:
+            print("Not Weird")
+            
+#Python: Division
+from __future__ import division
+
+if __name__ == '__main__':
+    a = int(raw_input())
+    b = int(raw_input())
+    print(a//b)
+    print(a/b)
+        
