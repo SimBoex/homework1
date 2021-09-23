c5a21ff21783cb09a089fcee428060f291f783fe
SimBoex <91120377+SimBoex@users.noreply.github.com>
2021-09-23 23:54:36 +0200

    Update scripts.py

--- scripts.py
+++ scripts.py
@@ -53,3 +53,13 @@ def is_leap(year):
     return leap
 
 year = int(raw_input())
+
+#Print Function
+from __future__ import print_function
+
+if __name__ == '__main__':
+    n = int(raw_input())
+    s=""
+    for i in range(1,n+1):
+        s=s+str(i)
+    print(s)
