59501d0d64dc19077a7eb80da58ce8858e4fbbe8
SimBoex <91120377+SimBoex@users.noreply.github.com>
2021-09-26 15:49:41 +0200

    Update scripts.py

--- scripts.py
+++ scripts.py
@@ -159,3 +159,9 @@ if __name__ == '__main__':
              list.reverse()
         x+=1
         
+#Tuples
+if __name__ == '__main__':
+    n = int(raw_input())
+    integer_list = map(int, raw_input().split())
+    t=tuple(integer_list)
+    print(hash(t))
