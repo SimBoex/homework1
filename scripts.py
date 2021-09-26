5403811dfe2adc4a474c3026804e71903edead02
SimBoex <91120377+SimBoex@users.noreply.github.com>
2021-09-26 16:10:21 +0200

    Update scripts.py

--- scripts.py
+++ scripts.py
@@ -186,3 +186,12 @@ if __name__ == '__main__':
     result = split_and_join(line)
     print result
     
+#What's Your Name?
+def print_full_name(first, last):
+    # Write your code here
+    print("Hello {} {}! You just delved into python.".format(first,last))
+
+if __name__ == '__main__':
+    first_name = raw_input()
+    last_name = raw_input()
+    print_full_name(first_name, last_name)
