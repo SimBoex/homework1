0b1163e50be8ec3e402b9630e4bc1c768fe1738f
SimBoex <91120377+SimBoex@users.noreply.github.com>
2021-09-26 16:18:17 +0200

    Update scripts.py

--- scripts.py
+++ scripts.py
@@ -195,3 +195,14 @@ if __name__ == '__main__':
     first_name = raw_input()
     last_name = raw_input()
     print_full_name(first_name, last_name)
+    
+#Mutations
+def mutate_string(string, position, character):
+    string=string[0:position]+character+string[position+1:]
+    return string
+
+if __name__ == '__main__':
+    s = raw_input()
+    i, c = raw_input().split()
+    s_new = mutate_string(s, int(i), c)
+    print s_new
