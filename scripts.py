bcab490ce5a7200799e9792dc372a4142c10c7b2
SimBoex <91120377+SimBoex@users.noreply.github.com>
2021-10-04 23:03:58 +0200

    Update scripts.py

--- scripts.py
+++ scripts.py
@@ -842,6 +842,25 @@ for _ in range(n):
 t.strip()
 parser.feed(t) 
 
+
+#XML 1 - Find the Score
+import sys
+import xml.etree.ElementTree as etree
+
+def get_attr_number(node):
+    # your code goes here
+    cont=0
+    for child in node.iter():
+        cont+=len(child.attrib)
+    return cont
+
+if __name__ == '__main__':
+    sys.stdin.readline()
+    xml = sys.stdin.read()
+    tree = etree.ElementTree(etree.fromstring(xml))
+    root = tree.getroot()
+    print(get_attr_number(root))
+
 #Arrays
 import numpy
 
