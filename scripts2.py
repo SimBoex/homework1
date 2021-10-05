b789f09cead342b28abc22f8b5b3eb221a6da027
SimBoex <91120377+SimBoex@users.noreply.github.com>
2021-10-05 20:58:15 +0200

    Update scripts2.py

--- scripts2.py
+++ scripts2.py
@@ -1,1 +1,40 @@
 
+#Birthday Cake Candles
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'birthdayCakeCandles' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY candles as parameter.
+#
+def birthdayCakeCandles(candles):
+    # Write your code here
+    cont=0
+    max=0
+    for el in candles:
+        if max<el:
+            max=el
+            cont=1
+        elif max==el:
+            cont+=1
+    return cont
+    
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    candles_count = int(input().strip())
+
+    candles = list(map(int, input().rstrip().split()))
+
+    result = birthdayCakeCandles(candles)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
