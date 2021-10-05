f706a82ec8082d5ad5043698310e7b6f6b65080c
SimBoex <91120377+SimBoex@users.noreply.github.com>
2021-10-05 21:34:05 +0200

    Update scripts2.py

--- scripts2.py
+++ scripts2.py
@@ -38,3 +38,69 @@ if __name__ == '__main__':
     fptr.write(str(result) + '\n')
 
     fptr.close()
+
+    
+    
+    
+#Number Line Jumps
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'kangaroo' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts following parameters:
+#  1. INTEGER x1
+#  2. INTEGER v1
+#  3. INTEGER x2
+#  4. INTEGER v2
+#
+
+def kangaroo(x1, v1, x2, v2):
+    # Write your code here
+    if v1<v2:
+        slower=v1
+        faster=v2
+    else:
+        slower=v2
+        faster=v1
+    if x1<x2:
+        far=x1
+        ahead=x2
+    else:
+        far=x2
+        ahead=x1
+    if (far==x1 and slower==v1):
+        return "NO"
+    if (far==x2 and slower==v2):
+        return "NO"
+    while far<ahead:
+        far+=faster
+        ahead+=slower
+        if far==ahead:
+            return "YES"
+    return "NO"
+    
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    x1 = int(first_multiple_input[0])
+    v1 = int(first_multiple_input[1])
+
+    x2 = int(first_multiple_input[2])
+
+    v2 = int(first_multiple_input[3])
+
+    result = kangaroo(x1, v1, x2, v2)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
