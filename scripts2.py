1cf0a1ae0559178a1625a02c747ffd614a32da9b
SimBoex <91120377+SimBoex@users.noreply.github.com>
2021-10-06 22:32:59 +0200

    Update scripts2.py

--- scripts2.py
+++ scripts2.py
@@ -231,3 +231,37 @@ if __name__ == '__main__':
     arr = list(map(int, input().rstrip().split()))
 
     insertionSort1(n, arr)
+    
+#Insertion Sort - Part 2
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'insertionSort2' function below.
+#
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER_ARRAY arr
+#
+
+def insertionSort2(n, arr):
+    # Write your code here
+    for pos in range(1,n): 
+        value=arr[pos]
+        x=pos-1
+        while arr[x]>value and x>=0:
+            arr[x+1]=arr[x]
+            arr[x]=value
+            x-=1
+        print(*arr)
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    insertionSort2(n, arr)
